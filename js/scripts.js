1fdd140f26ac35d740290426aff258baeaa8906f
Collin Smith <collin.smith@iostudio.com>
2019-12-08 13:46:50 -0600

    limit actions in done function, create user instance, and use a single modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,100 @@
-//AJAX Request
+// AJAX Request
+let users = [];
 $.ajax({
   url: "https://randomuser.me/api/?results=12",
   method: "GET",
   dataType: "json",
   success: response => {
-    randUserGen = response.results;
+    let randUsers = response.results;
+    randUsers.forEach(details => {
+      let user = new User(details);
+      users.push(user);
+    });
   }
 }).done(() => {
-  //gallery creation and appendage
-  randUserGen.forEach(user => {
-    let thumbPhoto = user.picture.large;
-    let userName = `${user.name.first} ${user.name.last}`;
-    let email = user.email;
-    let location = `${user.location.city}, ${user.location.state}`;
-    let street = `${user.location.street.number} ${user.location.street.name}`;
-    let userID = user.login.username;
-    let phone = user.phone;
-    let dob = user.dob.date;
-    let dobFiltered = dob.slice(0, 10);
-    let detailedLocation = `${street} ${location} ${user.location.postcode}`;
-    //HTML construction for gallery
-    let userHtml = `<div class="card ${userID}">
-        <div class="card-img-container">
-            <img class="card-img" src=${thumbPhoto} alt="profile picture">
-        </div>
-        <div class="card-info-container">
-            <h3 id="name" class="card-name cap">${userName}</h3>
-            <p class="card-text">${email}</p>
-            <p class="card-text cap">${location}</p>
-        </div>
+  // gallery creation and appendage
+  addUsers(users);
+  $("#gallery").after(`<div id="modal" class="modal-container hide"></div>`);
+});
+
+class User {
+  constructor(details) {
+    this.details = details;
+    //
+    this.id = details.login.username;
+    this.photo = details.picture.large;
+    this.name = `${details.name.first} ${details.name.last}`;
+    this.email = details.email;
+    this.phone = details.phone;
+    this.dob = details.dob.date.slice(0, 10);
+    this.location = `${details.location.city}, ${details.location.state}`;
+    this.street = `${details.location.street.number} ${
+      details.location.street.name
+    }`;
+    this.detailed = `${this.street} ${this.location} ${
+      details.location.postcode
+    }`;
+  }
+
+  // generate a card layout with user details
+  generateCard() {
+    let userHtml = `<div class="card ${this.id}">
+      <div class="card-img-container">
+        <img class="card-img" src=${this.photo} alt="profile picture">
+      </div>
+      <div class="card-info-container">
+        <h3 id="name" class="card-name cap">${this.name}</h3>
+        <p class="card-text">${this.email}</p>
+        <p class="card-text cap">${this.location}</p>
+      </div>
     </div>`;
-
-    //HTML appendage for gallery
+    // append results
     $("#gallery").append(userHtml);
+  }
 
-    //modal HTML construction
-    let modalHtml = `
-    <div class="modal-container hide ${userID}">
-      <div class="modal">
-          <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-          <div class="modal-info-container">
-              <img class="modal-img" src=${thumbPhoto} alt="profile picture">
-              <h3 id="name" class="modal-name cap">${userName}</h3>
-              <p class="modal-text">${email}</p>
-              <p class="modal-text cap">${detailedLocation}</p>
-              <hr>
-              <p class="modal-text">${phone}</p>
-              <p class="modal-text">${location}</p>
-              <p class="modal-text">Birthday: ${dobFiltered}</p>
-            </div>
-      </div>`;
-    $("#gallery").after(modalHtml);
-    //modal HTML appendage
-  });
+  // set modal to active user using a template that utilizes contructor data
+  setModal() {
+    let modalHtml = `<div class="modal ${this.id}">
+      <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+      <div class="modal-info-container">
+        <img class="modal-img" src=${this.photo} alt="profile picture">
+        <h3 id="name" class="modal-name cap">${this.name}</h3>
+        <p class="modal-text">${this.email}</p>
+        <p class="modal-text cap">${this.detailed}</p>
+        <hr>
+        <p class="modal-text">${this.phone}</p>
+        <p class="modal-text">${this.location}</p>
+        <p class="modal-text">Birthday: ${this.dob}</p>
+      </div>
+    </div>`;
+    // set the modal
+    $("#modal")
+      .empty()
+      .append(modalHtml)
+      .addClass("show");
+  }
+}
 
-  // on click to show user modal
-  $(".card").on("click", function(e) {
-    let selectedUserID =
-      "." +
-      $(this)
-        .attr("class")
-        .split(" ")[1];
-    $(selectedUserID).addClass("show");
+// method to add users to the DOM
+const addUsers = users => {
+  users.forEach(user => {
+    user.generateCard();
   });
+};
 
-  //on click of exit button on modal to hide modal window
-  $(".modal-close-btn").on("click", function(e) {
-    $(".modal-container").removeClass("show");
-  });
+// set the modal by finding which user was clicked and invoking instance method
+$("#gallery").on("click", ".card", e => {
+  const that = $(e.currentTarget);
+  const id = that.attr("class").split(" ")[1];
+  let user = users.filter(user => {
+    return user.id === id;
+  })[0];
+  user.setModal();
+});
+
+// hide the modal
+$(document).on("click", "#modal, #modal-close-btn", () => {
+  $("#modal")
+    .removeClass("show")
+    .addClass("hide");
 });
